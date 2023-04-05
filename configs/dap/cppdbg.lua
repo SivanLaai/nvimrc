b48e8985eba976db43c4fbc25c4673a74835160b
SivanLaai <lyhhap@163.com>
2023-04-05 19:39:17 +0800

    add debug mode for cpp and python

---
+++ configs/dap/cppdbg.lua
@@ -0,0 +1,36 @@
+local dap  = require("dap")
+local function isempty(s)
+	return s == nil or s == ""
+end
+dap.adapters.cppdbg = {
+  id = 'cppdbg',
+  type = 'executable',
+  command = '/home/ccip/.local/share/nvim/mason/bin/OpenDebugAD7',
+}
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopAtEntry = true,
+  },
+  {
+    name = 'Attach to gdbserver :1234',
+    type = 'cppdbg',
+    request = 'launch',
+    MIMode = 'gdb',
+    miDebuggerServerAddress = 'localhost:1234',
+    miDebuggerPath = '/usr/bin/gdb',
+    cwd = '${workspaceFolder}',
+    args = function()
+      return {vim.fn.input('Parameters to executable: ', vim.fn.getcwd() .. '/', 'file')}
+    end,
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+  },
+}
--- mappings.lua
+++ mappings.lua
@@ -1,12 +1,27 @@
 ---@type MappingsTable
 local M = {}
 
-M.general = {
+M.Spectre = {
   n = {
+    -- spectre
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<leader>S"] = { ":lua require('spectre').open()<CR>", "open spectre windows" },
     ["<leader>sw"] = { ":lua require('spectre').open_visual({select_word=true})<CR>", "spectre search current word" },
     ["<leader>sf"] = { ":lua require('spectre').open_file_search({select_word=true})<CR>", "spectre search on current file" },
+  },
+}
+M.Debug = {
+  n = {
+    ["<F5>"] = { ':lua require("dap").continue()<CR>', "debug: run/continue" },
+    ["<F7>"] = { ':lua require("dap").toggle_breakpoint()<CR>', "debug: toggle breakpoint" },
+    ["<F8>"] = { ':lua require("dap").terminate() require("dapui").close()<CR>', "debug: stop" },
+    ["<F9>"] = { ':lua require("dap").step_into()<CR>', "debug: step into" },
+    ["<F10>"] = { ':lua require("dap").step_out()<CR>', "debug: step out" },
+    ["<F11>"] = { ':lua require("dap").step_over()<CR>', "debug: step out" },
+    ["<leader>db"]= { ':lua require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>', "debug: Set breakpoint with condition" },
+    ["<leader>dc"]= { ':lua require("dap").run_to_cursor()<CR>', "debug: run to cursor" },
+    ["<leader>dl"]= { ':lua require("dap").run_last()<CR>', "debug: run last" },
+    ["<leader>do"]= { ':lua require("dap").repl.open()<CR>', "debug: open REPL" },
   },
 }
 
